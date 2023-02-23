74bee14e2ace837c6d7f260e7af4962fb0f6b6da
ajayjagtap-1404 <ajayjagtap1404@gmail.com>
2023-02-23 14:35:54 +0530

    Start code for Line Comparison Program

---
+++ LineProgram.java
@@ -0,0 +1,7 @@
+package com.assigenement.day_3.line_comparision;
+
+public class LineProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison  Program");
+    }
+}
